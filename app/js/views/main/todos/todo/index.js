a84960a1edc0bf805d052588220614e2a9f1a78c
Craig Condon <craig@spiceapps.com>
2014-06-09 17:33:03 -0700

    update docs

--- app/js/views/main/todos/todo/index.js
+++ app/js/views/main/todos/todo/index.js
@@ -1,4 +1,8 @@
 var views = require("mojo-views");
 module.exports = views.Base.extend({
-  paper: require("./index.pc")
+  paper: require("./index.pc"),
+  save: function () {
+    this.set("edit", false);
+    this.get("model").save();
+  }
 });
